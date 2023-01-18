e01d3eeaa3092be828f355c9e9d26adb9db61950
Tony <bbellavi@student.42.fr>
2023-01-18 12:45:46 +0100

    Add: WIP on pretty-print-bookmarks, added goal formatting with *format-padding* binding

--- src/bookmark.clj
+++ src/bookmark.clj
@@ -1,10 +1,13 @@
 (ns bookmark)
 
-(defn -get-pages-per-day
+(def ^{:dynamic true, :private true}
+  *format-padding* -15)
+
+(defn- -get-pages-per-day
   [total duration]
   (int (/ total duration)))
 
-(defn -get-bookmarks-list
+(defn- -get-bookmarks-list
   [begin end duration]
   (let [pages-per-day (-get-pages-per-day (- end begin) duration)]
     (loop [current (+ begin pages-per-day)
@@ -14,7 +17,7 @@
         (recur (+ current pages-per-day)
                (conj bookmarks current))))))
  
-(defn -get-bookmarks-statistics
+(defn- -get-bookmarks-statistics
   "Returns the statistics map composed of : bookmarks, pages per day, total pages"
   [begin end duration]
   (let [total-pages (- end begin)]
@@ -22,8 +25,22 @@
      :pages-per-day (-get-pages-per-day total-pages duration)
      :total-pages total-pages}))
 
+(defn- -format-goal
+  "Returns a string formatted with the description and the current goal.
+   the padding size can be overriden by the *format-padding* binding."
+  [desc goal]
+  (let [fmt (format "%%%ds %%d" *format-padding*)]
+    (format fmt desc goal)))
+
+(defn- -format-bookmarks-list
+  [bookmarks])
+
 (defn pretty-print-bookmarks
-  [stats])
+  [{:keys [bookmarks pages-per-day total-pages]}]
+  (format "%s\n%s\n%s"
+          (-format-goal "Daily goal: " pages-per-day)
+          (-format-goal "Total pages: " total-pages)
+          "TO REPLACE"))
 
 (defn bookmark
   "Returns a map with statistics about bookmarking a book specific duration"
