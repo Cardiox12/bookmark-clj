ad50641c015a46e037f663d75272a97f388223c4
Tony <bbellavi@student.42.fr>
2023-01-18 12:44:23 +0100

    Add: basic unit tests for bookmark function

--- test/bookmark_test.clj
+++ test/bookmark_test.clj
@@ -1,4 +1,20 @@
 (ns bookmark-test
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer :all]
+            [bookmark :refer :all]))
 
+(defmacro with-bookmark [begin end duration & body]
+  `(let [{:keys ~'[pages-per-day bookmarks total-pages]} (bookmark/bookmark ~begin ~end ~duration)]
+     (do ~@body)))
+
+(deftest bookmark-test
+  (testing "bookmark list length"
+    (with-bookmark 0 119 7
+      (is (= (count bookmarks) 7)))
+    (with-bookmark 0 119 1
+      (is (= (count bookmarks) 1))))
+  (testing "total pages"
+    (with-bookmark 0 119 7
+      (is (= total-pages 119)))
+    (with-bookmark 9 199 7
+      (is (= total-pages 190)))))
 
